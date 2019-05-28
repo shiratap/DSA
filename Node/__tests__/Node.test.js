5d4d40f0fc14512aa661e1d4a33088f90d85fe1d
shiratap <spencerhirata@gmail.com>
2019-05-28 14:14:22 -0700

    Deleted coverage from branch

--- Node/__tests__/Node.test.js
+++ Node/__tests__/Node.test.js
@@ -2,8 +2,25 @@
 
 const Node = require('../Node.js');
 
-describe('', () => {
-  it('', () => {
-    expect(true).toBeTruthy();
+describe('Node unit testing', () => {
+  const TestNode = new Node(1);
+
+  it('Test node should exist', () => {
+    expect(TestNode).toBeTruthy();
+  });
+  it('Test node should have a value of 1 if given 1 in the constructor', () => {
+    expect(TestNode.value).toEqual(1);
+  });
+  it('Should have a next value of null when instantiated', () => {
+    expect(TestNode.next).toBeNull();
+  });
+  it('Should have a Right Child value of null when instantiated', () => {
+    expect(TestNode.RightChild).toBeNull();
+  });
+  it('Should have a Left Child value of null when instantiated', () => {
+    expect(TestNode.LeftChild).toBeNull();
+  });
+  it('Should have a previous value of null when instantiated', () => {
+    expect(TestNode.previous).toBeNull();
   });
 });
