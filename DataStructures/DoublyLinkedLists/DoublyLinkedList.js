11bb43e5985334cfdbbca966c76b5c157feb15b8
shiratap <spencerhirata@gmail.com>
2019-10-05 10:52:56 -0700

    Small Doubly Linked List update

--- DataStructures/DoublyLinkedLists/DoublyLinkedList.js
+++ DataStructures/DoublyLinkedLists/DoublyLinkedList.js
@@ -1,1 +1,18 @@
 'use strict';
+
+module.exports = class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+  }
+  add(node) {
+    if (!this.head) {
+      this.head = node;
+    } else {
+      let curr = this.head;
+      while (curr.next !== null) {
+        curr = curr.next;
+      }
+      curr.next = node;
+    }
+  }
+};
